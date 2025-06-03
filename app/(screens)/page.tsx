2f39ca1b36420ac19d1e1e5f9849b6a4c2e10584
TimotejkoPantak <timopantakgit@gmail.com>
2025-06-03 11:18:59 +0200

    Create navbar and hero in root page

--- app/(screens)/page.tsx
+++ app/(screens)/page.tsx
@@ -6,10 +6,39 @@ export default function Home() {
 
   return (
     <>
-      <div className="flex justify-center items-center text-center bg-brand h-[100vh] text-white">
-        <h1 className="text-8xl font-bold font-playfair">Exclusive girls</h1>
-      </div>
-      <div className="flex p-2 md:p-4 lg:p-8">
+<div className="relative pt-[72px] h-[100vh] flex justify-center items-center flex-col text-center bg-[url(/bg_hero.jpg)] bg-bottom bg-cover bg-no-repeat text-white px-4 sm:px-6 lg:px-8">
+  {/* Overlay */}
+  <div className="absolute inset-0 bg-gradient-to-b from-black/70 to-transparent z-0"></div>
+
+  {/* Heading */}
+  <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-[52px] leading-tight font-bold font-playfair z-10 max-w-[90%] sm:max-w-[700px] lg:max-w-[1440px] text-center relative">
+  Výber exkluzívnych dievčat, <br className="hidden sm:block"/>
+  ktoré vám splnia vaše{' '}
+  <span className="relative inline-block">
+    erotické túžby
+    <img
+      src="/underline.svg"
+      alt=""
+      className="absolute left-1/2 -translate-x-1/2 bottom-0 translate-y-[60%] w-full pointer-events-none"
+    />
+  </span>
+</h1>
+
+
+  {/* Paragraph 1 */}
+  <p className="text-sm sm:text-base lg:text-[16px] font-parkinsans z-10 max-w-[90%] sm:max-w-[700px] lg:max-w-[920px] mt-4">
+    Ponúkame viac než len sprievod do spoločnosti, je to skutočný zážitok spojený s eleganciou, štýlom a vynikajúcou spoločnosťou. Ponúkame profesionálnu spoločnosť pre rôzne príležitosti, od firemných podujatí až po romantické večere plné erotiky a vášne. Eskort dievčatá, sú pripravené pre vás vytvoriť príjemnú atmosféru, ktorú si zaslúžite. Nie sú to len obyčajné ženy, ale sú to šarmantné dámy, ktoré presne vedia, ako sa majú správať a ako sa majú obliekať. Ich elegancia je príznačná nielen pre ich zovňajšok, ale aj pre ich správanie.
+  </p>
+
+  {/* Paragraph 2 */}
+  <p className="text-sm sm:text-base lg:text-[16px] font-parkinsans z-10 max-w-[90%] sm:max-w-[700px] lg:max-w-[920px] mt-4">
+    Diskrétnosť je ich prioritou, a tak je každý zážitok starostlivo chránený, pričom súkromie klienta je vždy na provom mieste. Neváhajte a vyberte si dievča podľa vašich predstáv. Nechajte sa zlákať svetom, kde elegancia, krása a vášeň sú na dennom poriadku, a kde vaša spoločníka je skutočná dáma, pripravená uspokojiť vaše najtajnejšie túžby a priania.
+  </p>
+</div>
+
+
+
+      <div className="flex  md:p-4 lg:p-8">
         {modelCards.length > 0 ? (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 max-w-[1440px] mx-auto justify-center">
             {modelCards.map((card) => {
---
+++ app/components/layout/Navbar.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="absolute p-4 w-full bg-black/60 after:content-[''] after:absolute after:bottom-0 after:left-0 after:w-full after:h-[2px] after:bg-[linear-gradient(to_right,_#ED217E,_#960346)] after:rounded-sm z-10">
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo - always visible */}
+        <Link href="/" className="text-white text-lg font-bold flex items-center gap-[8px]">
+          <img src="/logo_exclusive-girls.svg" alt="Logo" className="h-[32px]" />
+        </Link>
+
+        <div className="flex items-center gap-[4px]">
+          {/* Desktop links - hidden on mobile */}
+          <div className="hidden sm:flex flex-row gap-[4px] items-center">
+            <Link href="/" className="text-white hover:text-gray-300 font-parkinsans font-bold flex items-center gap-[4px]">
+              <img src="/icon_heart.svg" className="w-[24px] h-[24px]" />
+              <span className="text-[16px]">Obľúbené inzeráty</span>
+            </Link>
+
+            <Link href="/" className="text-white px-[24px] hover:text-gray-300 font-parkinsans font-bold flex items-center gap-[4px]">
+              <img src="/icon_user.svg" className="w-[24px] h-[24px]" />
+              <span>Prihlásiť sa</span>
+            </Link>
+
+            <Link href="/" className="text-white px-[24px] font-parkinsans font-bold bg-brand rounded-md h-[48px] flex items-center">
+              <span>Pridať inzerát</span>
+            </Link>
+          </div>
+
+          {/* Hamburger - always visible */}
+          <button className="text-white px-[24px] py-[17px] hover:text-gray-300 font-parkinsans font-bold flex items-center gap-[4px]">
+            <img src="/icon_hamburger-menu.svg" className="w-[24px] h-[24px]" />
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
